51dc2c277c13656e41353e9bc67ae7495a7f0ccf
MateuVieira <mateus12v@hotmail.com>
2018-12-04 15:48:16 -0200

    Update Day_2_Inventory_Management_System.py

--- Day 2 Inventory Management System/Day_2_Inventory_Management_System.py
+++ Day 2 Inventory Management System/Day_2_Inventory_Management_System.py
@@ -1,10 +1,5 @@
 def day_2(data):
-    '''
-    twice = twice_count(data)
-    three_times = three_times_count(data)
-    twice = {}
-    three_times = {}
-    '''
+   
     list_word = {2:0,3:0}
     for word in data:
         dic_word = {}
@@ -15,19 +10,7 @@ def day_2(data):
                 dic_word[letter] =1
         list_word[2] += count_value(dic_word,2)
         list_word[3] += count_value(dic_word,3)
-        #print(f'\n-list_word:\n--2, {list_word[2]}\n--3, {list_word[3]}\n------------------')
-
-    '''
-    print('Twice: \n')
-    print(twice)
-    print('\n ------------------------------')
-    print('three_times: \n')
-    print(three_times)
-    print('\n ------------------------------')
-    len_twice = len(twice)
-    len_three_times = len(three_times)
-    '''
-
+          
     print(f'Result: {list_word[2]} * {list_word[3]} = {list_word[2]*list_word[3]}')
 
 
@@ -38,41 +21,9 @@ def count_value(dic_word, num):
         
     return 0
 
-'''    
-def twice_count(data):
-    twice = {}
-    for word in data:
-        dic_word = {}
-        for letter in word:
-            if letter in dic_word:
-                dic_word[letter] +=1
-            else:
-                dic_word[letter] =1
-
-        for letter in word:   
-            if (dic_word[letter] == 2) and not (letter in twice):
-                twice[letter] = 1   
-    return twice         
-
-def three_times_count(data):
-    three_times = {}
-    for word in data:
-        dic_word = {}
-        for letter in word:
-            if letter in dic_word:
-                dic_word[letter] +=1
-                if (dic_word[letter] == 3) and not (letter in three_times):
-                    three_times[letter] = 1
-            else:
-                dic_word[letter] =1
-    return three_times
- '''
 
 f = open('day_2_input.txt','r')
 data_f = [x.strip('\n') for x in f.readlines()]
-
-#print(data)
-
 
 # Data -> Test
 data = ['abcdef','bababc','abbcde','abcccd','aabcdd','abcdee','ababab']
@@ -81,6 +32,3 @@ day_2(data)
 print('\n --------------\n')
 day_2(data_f)
 
-
-
-    
